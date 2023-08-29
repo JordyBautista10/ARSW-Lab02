10fc33cb25944704b339bd09ea74517680177b1d
Jordy <jordy.sbautista@gmail.com>
2023-08-28 23:04:53 -0500

    Intento 1

--- src/main/java/edu/eci/arsw/primefinder/Control.java
+++ src/main/java/edu/eci/arsw/primefinder/Control.java
@@ -5,6 +5,11 @@
  */
 package edu.eci.arsw.primefinder;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
 /**
  *
  */
@@ -17,17 +22,22 @@ public class Control extends Thread {
     private final int NDATA = MAXVALUE / NTHREADS;
 
     private PrimeFinderThread pft[];
+    private LinkedList<Integer> primes;
     
     private Control() {
         super();
         this.pft = new  PrimeFinderThread[NTHREADS];
+        this.primes  = new LinkedList<>();
 
         int i;
         for(i = 0;i < NTHREADS - 1; i++) {
-            PrimeFinderThread elem = new PrimeFinderThread(i*NDATA, (i+1)*NDATA);
-            pft[i] = elem;
+            PrimeFinderThread elem = new PrimeFinderThread(i*NDATA, (i+1)*NDATA, primes);
+            synchronized (primes) {
+                pft[i] = elem;
+            }
+
         }
-        pft[i] = new PrimeFinderThread(i*NDATA, MAXVALUE + 1);
+        pft[i] = new PrimeFinderThread(i*NDATA, MAXVALUE + 1,primes);
     }
     
     public static Control newControl() {
@@ -39,6 +49,21 @@ public class Control extends Thread {
         for(int i = 0;i < NTHREADS;i++ ) {
             pft[i].start();
         }
+        for(int i = 0;i < NTHREADS;i++ ) {
+            try {
+                pft[i].join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        System.out.println("----------------------------------------------------------------");
+        System.out.println(pft[0].getPrimes().size());
+        System.out.println(pft[1].getPrimes().size());
+        System.out.println(pft[2].getPrimes().size());
+        System.out.println("----------------------------------------------------------------");
+        System.out.println(pft[0].getPrimes().get(0));
+        System.out.println(pft[1].getPrimes().get(1));
+        System.out.println(pft[2].getPrimes().get(2));
     }
     
 }
